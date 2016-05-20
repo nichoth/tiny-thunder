d6fc71564c7f0f6f0cfa354fc710e31b22b10697
nichoth <nichoth@gmail.com>
2016-05-20 15:26:08 -0700

    delete old code

--- src/lib/data/order.js
+++ src/lib/data/order.js
@@ -9,41 +9,12 @@ module.exports = function(moltin, cartModel) {
   //var info = state ? JSON.parse(state) : { customer: {}, payment: {} }
 
   var s = struct({
-    // orderInfo: struct({
-    //   bill_to: {},
-    //   ship_to: {},
-    //   customer: {},
-    //   gateway: 'dummy',
-    //   shipping: shipping
-    // }),
-    // paymentInfo: struct({}),
     order: struct({}),
     status: struct({
       type: null
     })
   })
 
-  // function setOrderInfo(data) {
-  //   s.orderInfo.set(xtend({
-  //     ship_to: 'bill_to',
-  //     gateway: 'dummy',
-  //     shipping: shipping
-  //   }, data))
-  // }
-
-  // function setPaymentInfo(cardInfo) {
-  //   s.paymentInfo.set(cardInfo)
-  // }
-
-  // function pay(moltin, orderId, cardData, cb) {
-  //   moltin.Checkout.Payment('purchase', orderId, cardData,
-  //     function onSuccess(order) {
-  //       cb(null, order)
-  //     }, function onErr(err) {
-  //       cb(arguments)
-  //     }
-  //   )
-  // }
 
   function submitOrder(moltin, orderData, cb) {
     console.log(orderData)
@@ -82,9 +53,6 @@ module.exports = function(moltin, cartModel) {
   return {
     state: s,
     actions: {
-      //pay: pay.bind(null, moltin),
-      //setOrderInfo: setOrderInfo.bind(null),
-      //setPaymentInfo: setPaymentInfo.bind(null),
       submitOrder: submitOrder.bind(null, moltin)
     }
   }
