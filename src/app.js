31deb61e992f646dccb180836f6a7a53c376d69c
nichoth <nichoth@gmail.com>
2016-07-16 16:28:21 -0700

    almost working more

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var throttle = require('raf-listen')
 var addClass = require('dom101/add-class')
 var hasClass = require('dom101/has-class')
 var removeClass = require('dom101/remove-class')
+var outerHeight = require('dom101/outer-height')
 var moltin = new Moltin({
   publicId: process.env.PUBLIC_ID
 })
@@ -12,30 +13,38 @@ window.moltin = moltin
 
 module.exports = function() {
 
-  var el = render('')
-
   function listen () {
     var st = window.st = require('scrolltop')
-    var el = document.querySelector('nav.main-nav')
-    var offset = el.offsetTop
+    var navEl = document.querySelector('nav.main-nav')
+    var logoEl = document.querySelector('.logo-wrapper')
+    var offset = navEl.offsetTop
     var throttledScroll = throttle(onScroll)
-    window.addEventListener('scroll', throttledScroll)
+    var winHeight = window.innerHeight
+    var h = outerHeight(logoEl)
+    if (navEl) {
+      window.addEventListener('scroll', throttledScroll)
+    }
 
     function onScroll (ev) {
-      if ((st() - 20) >= offset) {
-        addClass(el, 'sticky')
+      var viewportHeight = outerHeight(logoEl)
+      var scrTop = st()
+      var navHeight = outerHeight(document.querySelector('.diamond-wrapper'))
+      if (scrTop >= (viewportHeight - navHeight + 5)) {
+        addClass(navEl, 'sticky')
       } else {
-        if (hasClass(el, 'sticky')) removeClass(el, 'sticky')
+        if (hasClass(navEl, 'sticky')) removeClass(navEl, 'sticky')
       }
+
     }
   }
+
+  var el = render('')
 
   moltin.Authenticate(function onSuccess(auth) {
     var router = Router(moltin)
     router(function onChange(content) {
       update(el, render(content))
       listen()
-      console.log(el.querySelector('nav.main-nav'))
     })
   }, function onErr(err) {
     console.log('arr', err)
