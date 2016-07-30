a84cb1678f9f0b5b8959dc21dfc51e1f25f052a9
nichoth <nichoth@gmail.com>
2016-07-30 15:19:53 -0700

    fix #19 - scroll position

--- src/lib/routes/index.js
+++ src/lib/routes/index.js
@@ -7,13 +7,18 @@ var Routes = require('./routes')
 module.exports = function(moltin) {
   var router = Router()
   var activeRoute = observ('')
-  var setRoute = onRoute(function(path) {
+
+  var setRoute = onRoute(function(path, scroll) {
     var m = router.match(path)
     if (!m) {
       return activeRoute.set(four04())
     }
     m.fn(m.params)
-  })
+    console.log(path, scroll);
+    setTimeout(function () {
+      window.scrollTo(scroll.scrollX, scroll.scrollY);
+    }, 0)
+  }, { saveScroll: true })
 
   var routes = Routes(moltin, setRoute)
 
@@ -30,6 +35,7 @@ module.exports = function(moltin) {
       stopListening = routeState(function onChange(data) {
         activeRoute.set(routeState.render(data))
       })
+
     })
   })
 
